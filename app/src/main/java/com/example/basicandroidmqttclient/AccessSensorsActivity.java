8c88d7a0594a204f7afcc777ce040c202dade09a
Fábio M. Costa <fmc@ufg.br>
2023-11-28 00:31:35 -0300

    Added access to light sensor

--- app/src/main/java/com/example/basicandroidmqttclient/AccessSensorsActivity.java
+++ app/src/main/java/com/example/basicandroidmqttclient/AccessSensorsActivity.java
@@ -4,26 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.List;
 
 public class AccessSensorsActivity extends AppCompatActivity {
 
+    private SensorManager sensorManager;
+    private Sensor mLight, mTemperature;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        SensorManager sensorManager;
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_access_sensors);
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        List<Sensor> deviceSensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
-        EditText sensorInfoField = (EditText) findViewById(R.id.editTextSensorInfo);
-        sensorInfoField.setText(deviceSensors.toString());
+        TextView textView = (TextView) findViewById(R.id.textViewLuminosity);
+        //mTemperature = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        //List<Sensor> deviceSensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        //EditText sensorInfoField = (EditText) findViewById(R.id.editTextSensorInfo);
+        //sensorInfoField.setText(deviceSensors.toString());
+
+        LightSensorAccess lightSensorAccess = new LightSensorAccess(sensorManager, textView);
+
 
     }
+
 }
+
